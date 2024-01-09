74ac6d030b04ab5754ef7b43d80a50e9c976b904
jovenlaurens <joven.nicholas@gmail.com>
2024-01-09 16:37:58 +0700

    FIxed navigation logo and projects

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ function stickyHeader() {
     const header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 0)
 }
+
+// Animation 
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the nav element
+    var nav = document.querySelector('.nav');
+
+    // Add the 'animate' class to trigger the animation only on the first load
+    nav.classList.add('animate');
+
+    // Remove the 'animate' class after the animation ends
+    nav.addEventListener('animationend', function() {
+        nav.classList.remove('animate');
+    });
+});
+
+// Scroll Reveal Animation
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+});
+
+// Scroll Home
+sr.reveal(".home-title", {});
+sr.reveal(".home-subtitle", { delay: 200 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".home-social-icon", { interval: 200 });
